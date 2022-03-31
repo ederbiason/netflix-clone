a98c13a870e3be5b40a9ec7b81ae96136e0f44f7
Eder Henrique Biason de Oliveira <82395795+ederbiason@users.noreply.github.com>
2022-03-31 11:41:01 -0300

    fix movie description

--- src/components/FeaturedMovie.js
+++ src/components/FeaturedMovie.js
@@ -16,6 +16,11 @@ export default ({item}) => {
         genres.push(item.genres[i].name)
     }
 
+    let description = item.overview;
+    if (description.length > 200) {
+        description = description.substring(0, 200)+'...'
+    }   
+
     return (
         <section className="featured" style={{
             backgroundSize: 'cover',
@@ -31,7 +36,7 @@ export default ({item}) => {
                         <div className="featured--seasons">{item.number_of_seasons} season{item.number_of_seasons !== 1 ? 's' : ''}</div>
                     </div>
 
-                    <div className="featured--description">{item.overview}</div>
+                    <div className="featured--description">{description}</div>
 
                     <div className="featured--buttons">
                         <a href={`/watch/${item.id}`} className="featured--watchbutton">► Watch</a>
