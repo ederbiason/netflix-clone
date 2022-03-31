fdbd9a4d2941dea0bc6d7e8a2d2a3d6e18771078
Eder Henrique Biason de Oliveira <82395795+ederbiason@users.noreply.github.com>
2022-03-31 10:42:57 -0300

    css navigation list

--- src/components/MovieRow.js
+++ src/components/MovieRow.js
@@ -2,11 +2,22 @@
 /* eslint-disable import/no-anonymous-default-export */
 import React from 'react';
 import './MovieRow.css';
+import NavigateBeforeIcon from '@material-ui/icons/NavigateBefore';
+import NavigateNextIcon from '@material-ui/icons/NavigateNext';
 
 export default ({title, items}) => {
     return (
         <div className="movieRow">
             <h2>{title}</h2>
+
+            <div className="movieRow--left">
+                <NavigateBeforeIcon style={{fontSize: 50}} className=""/>
+            </div>
+
+            <div className="movieRow--right">
+                <NavigateNextIcon style={{fontSize: 50}} className=""/>
+            </div>
+
             <div className="movieRow--listarea">
                 <div className="movieRow--list">
                     {items.results.length > 0 && items.results.map((item, key) => (
